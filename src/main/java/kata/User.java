fc13b8112cd1aa31287bd57068827dc98fe4020d
Eric Macy <eric_macy@nonik.com>
2020-06-01 08:42:22 -0400

    Create User class

---
+++ src/main/java/kata/User.java
@@ -0,0 +1,14 @@
+package kata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    private String name;
+
+    public String getName() { return this.name; }
+
+    public User(String Name) {
+        name = Name;
+    }
+}
