805480f7d380bccce0c59362ea713e0095f5ff06
Eric Macy <eric_macy@nonik.com>
2020-06-01 08:46:40 -0400

    User Following method

--- src/main/java/kata/User.java
+++ src/main/java/kata/User.java
@@ -5,10 +5,15 @@ import java.util.Map;
 
 public class User {
     private String name;
+    private Map<String, User> following = new HashMap<String, User>();
 
     public String getName() { return this.name; }
 
     public User(String Name) {
         name = Name;
+    }
+
+    public void followUser(User user) {
+        following.put(user.getName(), user);
     }
 }
