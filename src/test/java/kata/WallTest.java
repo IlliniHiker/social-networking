58391f19dfb4acbc44bbc0bcd79c1e75ee803a0a
Eric Macy <eric_macy@nonik.com>
2020-06-01 09:28:40 -0400

    Wall test update

--- src/test/java/kata/WallTest.java
+++ src/test/java/kata/WallTest.java
@@ -1,4 +1,15 @@
 package kata;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WallTest {
+    @Test
+    void testAddUserToWall() {
+        String userName = "Alice";
+        User user1 = new User(userName);
+
+        assertEquals();
+    }
 }
