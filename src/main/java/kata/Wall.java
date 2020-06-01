130c20b1a5c89640ee930ea4ad9cad31bcc69ffd
Eric Macy <eric_macy@nonik.com>
2020-06-01 09:50:43 -0400

    addMessage to Wall class

--- src/main/java/kata/Wall.java
+++ src/main/java/kata/Wall.java
@@ -4,13 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Wall {
-    private static final Map<String, User> users = new HashMap<String, User>();
+    private static Map<String, User> users = new HashMap<String, User>();
+    private static Map<Integer, Message> messages = new HashMap<Integer, Message>();
 
     private Wall() {}
 
     public static Map<String, User> getUsers() { return users; }
+    public static Map<Integer, Message> getMessages() { return messages; }
 
     public static void addUser(User user) {
         users.put(user.getName(), user);
+    }
+
+    public static void addMessage(Message message) {
+        messages.put(message.getId(), message);
     }
 }
