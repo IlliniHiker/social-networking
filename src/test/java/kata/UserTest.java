568c9bf0f3ad1c8c629e3b9fc43d2945b0447bd0
Eric Macy <eric_macy@nonik.com>
2020-06-01 08:43:51 -0400

    User Following Test created

--- src/test/java/kata/UserTest.java
+++ src/test/java/kata/UserTest.java
@@ -11,4 +11,14 @@ public class UserTest {
         User user1 = new User(userName);
         assertEquals(userName, user1.getName());
     }
+
+    @Test
+    void testUserFollowing() {
+        String userName1 = "Alice";
+        String userName2 = "Bob";
+        User user1 = new User(userName1);
+        User user2 = new User(userName2);
+        user1.followUser(user2);
+        assertEquals(user2, user1.getFollowing().get(userName2));
+    }
 }
