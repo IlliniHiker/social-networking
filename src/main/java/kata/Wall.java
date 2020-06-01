c2194c149f50ac863b3b13e92e652b1c3f1b7494
Eric Macy <eric_macy@nonik.com>
2020-06-01 10:22:36 -0400

    some code refactoring final submission

--- src/main/java/kata/Wall.java
+++ src/main/java/kata/Wall.java
@@ -12,11 +12,11 @@ public class Wall {
     public static Map<Integer, User> getUsers() { return users; }
     public static Map<Integer, Message> getMessages() { return messages; }
 
-    public static void addUser(User user) {
+    protected static void addUser(User user) {
         users.put(user.getId(), user);
     }
 
-    public static void addMessage(Message message) {
+    protected static void addMessage(Message message) {
         messages.put(message.getId(), message);
     }
 }
